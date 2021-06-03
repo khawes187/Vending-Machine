ce4ae845cbd329d7af33cf3f83de74d06590ad55
Joshua Abrams <abramsj14@gmail.com>
2021-06-03 14:52:40 -0400

    menu options added

---
+++ Capstone/MenuOptions/MainMenu.cs
@@ -0,0 +1,65 @@
+﻿using Capstone.Classes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone.MenuOptions
+{
+    /// <summary>
+    /// The main menu that allows the user to select the items they want and lets them purchase said items
+    /// </summary>
+    public class MainMenu
+    {
+        public VendingMachine VM { get; }
+
+        public MainMenu(VendingMachine vm)
+        {
+            this.VM = vm;
+        }
+        public void DisplayMainMenu()
+        {
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.WriteLine("1) Display Vending Machine Items");
+                Console.WriteLine("2) Purchase");
+                Console.WriteLine("3) Exit");
+                Console.Write("Please Enter Selection: ");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Dictionary<string, VendingMachineItems> items = VM.Inventory;
+                        DisplayItems(items);
+                        Console.WriteLine("\nPress Enter to Return to The Main Manu Selection.");
+                        break;
+
+                }
+            }
+        }
+        public void DisplayItems(Dictionary<string, VendingMachineItems> items)
+        {
+            Console.WriteLine("\n\t\t-----------------------------------------------");
+            Console.WriteLine("\t\t|{0, 0} | {1, -20} | {2, 5} | {3, 8} |", "ID", "ITEM", "PRICE", "REMAINING");
+            Console.WriteLine("\t\t-----------------------------------------------");
+            foreach (KeyValuePair<string, VendingMachineItems> kvp in items)
+            {
+                string quantity = "";
+
+                if (kvp.Value.Quantity == 0)
+                {
+                    quantity = "Sold Out";
+                }
+                else
+                {
+                    quantity = kvp.Value.Quantity.ToString();
+                }
+                Console.WriteLine($"\t\t|{kvp.Key,0} | {kvp.Value.ProductName,20} | {kvp.Value.Price,5} | {quantity,8} |");
+            }
+            Console.WriteLine("\t\t-----------------------------------------------");
+        }
+
+
+    }
+}
---
+++ Capstone/MenuOptions/PurchaseMenu.cs
@@ -0,0 +1,126 @@
+﻿using Capstone.Classes;
+using Capstone.MenuOptions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Capstone
+{
+    /// <summary>
+    /// Purchase menu is a Main menu which guides the user through the purchase process flow
+    /// of adding money, selecting their option(s), and finishing the transaction.
+    /// </summary>
+    public class PurchaseMenu : MainMenu
+    {
+        #region Constructor
+        public PurchaseMenu(VendingMachine vm) : base(vm)
+        {
+        }
+        #endregion
+
+        #region Methods
+        public void DisplayPurchaseMenu()
+        {
+            string input;
+            bool keepGoing = true;
+            while (keepGoing)
+            {
+                Console.Clear();
+
+                Console.WriteLine("1) Feed Money");
+                Console.WriteLine("2) Select Product");
+                Console.WriteLine("3) Finish Transaction");
+                Console.WriteLine("\n");
+
+                Console.WriteLine($"Current Money Provided: {VM.Balance:c}");
+                Console.Write("Please Enter Selection: ");
+                input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1": //Feedmoney
+                        GetUsersPayment();
+                        continue;
+                    case "2": //Select product
+                        Dictionary<string, VendingMachineItems> items = VM.Inventory;
+                        DisplayItems(items);
+                        SelectItem(items);
+                        break;
+                    case "3": //Finish Transaction
+                        ChangeCalculator change = new ChangeCalculator(VM.Balance);
+                        VM.MakeChange();
+                        PrintChange(change);
+                        keepGoing = false;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid Menu Option. Please Try Again.");
+                        continue;
+                }
+            }
+        }
+
+        public void SelectItem(Dictionary<string, VendingMachineItems> items)
+        {
+            Console.Write("Please select item: ");
+            string selection = Console.ReadLine().ToUpper();
+
+            if (!items.ContainsKey(selection))
+            {
+                Console.WriteLine("Invalid Selection. Try Again.");
+            }
+            else if (items[selection].Quantity < 1)
+            {
+                Console.WriteLine("Out of stock. Please make a different selection.");
+            }
+            else if (VM.Balance < items[selection].Price)
+            {
+                Console.WriteLine($"You have insufficient funds. Please try again");
+
+            }
+            else
+            {
+                VM.DispenseItems(selection);
+                DisplayDispenseMessage(selection, items[selection]);
+            }
+        }
+
+        public void DisplayDispenseMessage(string selection, VendingMachineItems item)
+        {
+            Console.Clear();
+            Console.WriteLine($"{item.ProductName} is yours for only {item.Price:C}.");
+            Console.WriteLine($"{item.PurchaseMessage}");
+            Console.WriteLine($"You have {VM.Balance:C} remaining.");
+            Console.WriteLine($"Press Enter to Return to Menu.");
+        }
+
+
+        public void GetUsersPayment()
+        {
+            Console.Write("Please deposit money. (Whole Dollar Only): ");
+            string input = Console.ReadLine();
+
+
+
+            if (!int.TryParse(input, out int value))
+            {
+                Console.WriteLine($"Feed error. Please enter a whole dollar amount.");
+            }
+            if (value < 0)
+            {
+                Console.WriteLine($"Feed error. Please enter a whole dollar amount.");
+            }
+            else
+            {
+                VM.FeedMoney(value);
+                Console.WriteLine($"{value:C} added.");
+            }
+            Console.ReadLine();
+        }
+
+        public void PrintChange(ChangeCalculator change)
+        {
+            Console.WriteLine($"Here is your change: {change.Quarters} Quarters, {change.Dimes} Dimes, {change.Nickels} Nickels, and {change.Pennies} Pennies");
+        }
+        #endregion
+    }
+}

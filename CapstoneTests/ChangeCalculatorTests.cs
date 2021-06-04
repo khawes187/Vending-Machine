e1f5feb840b72548e3784daf9e3fff66609f5457
Joshua Abrams <abramsj14@gmail.com>
2021-06-04 10:14:33 -0400

    change calculator tests

---
+++ CapstoneTests/ChangeCalculatorTests.cs
@@ -0,0 +1,65 @@
+﻿using Capstone.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CapstoneTests
+{
+    [TestClass]
+    public class ChangeCalculatorTests
+    {
+        [DataTestMethod]
+        public void ZeroDollarsReturnsZeroChange()
+        {
+            ChangeCalculator change = new ChangeCalculator(0M);
+
+            Assert.AreEqual(0, change.Pennies);
+            Assert.AreEqual(0, change.Nickels);
+            Assert.AreEqual(0, change.Dimes);
+            Assert.AreEqual(0, change.Quarters);
+        }
+
+        [TestMethod]
+        public void SmallChangeTests()
+        {
+            ChangeCalculator change = new ChangeCalculator(0.03M);
+
+            Assert.AreEqual(3, change.Pennies);
+            Assert.AreEqual(0, change.Nickels);
+            Assert.AreEqual(0, change.Dimes);
+            Assert.AreEqual(0, change.Quarters);
+
+            change = new ChangeCalculator(0.17M);
+
+            Assert.AreEqual(2, change.Pennies);
+            Assert.AreEqual(1, change.Nickels);
+            Assert.AreEqual(1, change.Dimes);
+            Assert.AreEqual(0, change.Quarters);
+
+            change = new ChangeCalculator(0.60M);
+
+            Assert.AreEqual(0, change.Pennies);
+            Assert.AreEqual(0, change.Nickels);
+            Assert.AreEqual(1, change.Dimes);
+            Assert.AreEqual(2, change.Quarters);
+
+            change = new ChangeCalculator(1.99M);
+            Assert.AreEqual(4, change.Pennies);
+            Assert.AreEqual(0, change.Nickels);
+            Assert.AreEqual(2, change.Dimes);
+            Assert.AreEqual(7, change.Quarters);
+        }
+
+        [TestMethod]
+        public void LargeChangeTests()
+        {
+            ChangeCalculator change = new ChangeCalculator(105.69M);
+
+            Assert.AreEqual(4, change.Pennies);
+            Assert.AreEqual(1, change.Nickels);
+            Assert.AreEqual(1, change.Dimes);
+            Assert.AreEqual(422, change.Quarters);
+        }
+    }
+}
